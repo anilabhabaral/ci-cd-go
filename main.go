ed1a19e56a432eb15ca1d83995cecb9411f5e074
Anilabha Baral <85929177+anilabhabaral@users.noreply.github.com>
2024-04-05 23:56:38 +0530

    Update main.go

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func Api() {
 
 	m := martini.Classic()
 	m.Get("/greet", func() string {
-		return "Hello anil! \n"
+		return "Hello world! \n"
 	})
 	m.Get("/hello/:name", func(params martini.Params) string {
 		return "Hello " + PrintName(params["name"]) + "\n"
